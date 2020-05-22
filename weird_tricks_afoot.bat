8a58316c084430e2fc447f07b1d66059d5c917bd
xbl3 <58650872+xbl3@users.noreply.github.com>
2020-05-22 03:26:59 -0400

    )()()()()( oh winders

---
+++ weird_tricks_afoot.bat
@@ -0,0 +1,48 @@
+@echo off
+)a
+)
+)
+)
+)
+)
+)
+)
+)
+)
+)
+)
+)
+)
+)
+)
+)
+)
+)
+)
+)
+
+)(
+echo:)(
+)(
+set lsf_flags= --fast-list --use-mmap --no-traverse --skip-links --absolute --no-update-modtime --ignore-case 
+)(	set lsf_DIRS= --dirs-only %lsf_FLAGS%
+)(	set lsf_FILES= --files-only %lsf_FLAGS%
+)(	
+		set lsf_DEPTH= 
+)(
+)(
+)(
+)(
+echo:(%%%%%%%%%)
+echo:)%%%%%%%%%(
+echo:%lsf_flags%
+echo:%lsf_DIRS%
+echo:%lsf_FILES%
+)()()()()()(
+)
+)
+)
+)
+)
+)
+)
